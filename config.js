f020268a26a7876b4605eb1106857c6c2d599af5
Sk imran <ssk284582@gmail.com>
2025-12-02 09:57:41 +0530

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    BOT_TOKEN: "8407901110:AAEEneZr119DbdiRCnP5xqaAgnzLzKbLuIM",
+    ADMIN_ID: "5478832701",
+    DAILY_WITHDRAW_LIMIT: 1,
+    MIN_WITHDRAW: 10,
+    MAX_WITHDRAW: 10,
+    JOIN_BONUS: 5,
+    REF_BONUS: 5,
+    WEBSITE_URL: process.env.WEBSITE_URL || "http://localhost:3000",
+    BOT_USERNAME: "Testinguu8678_bot"
+};
